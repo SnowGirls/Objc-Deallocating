3aa63dfade47aefb2f455695b458960774cfa3a8
jun.shen <shenj6@xiaopeng.com>
2023-02-16 14:54:42 +0800

    update resource

--- Objc-Deallocating/ViewController.h
+++ Objc-Deallocating/ViewController.h
@@ -3,6 +3,4 @@
 
 @interface ViewController : UIViewController
 
-
 @end
-
--- Objc-Deallocating/ViewController.m
+++ Objc-Deallocating/ViewController.m
@@ -19,12 +19,12 @@ bool _objc_rootIsDeallocating(id _Nonnull obj);
     // Do any additional setup after loading the view.
     
     // Buttons
-    UIButton* button = [UIButton buttonWithType:UIButtonTypeSystem];
-    button.frame = CGRectMake(0, 100, 380, 50);
-    [button setTitle:@"Click Me Crash on objc_initWeak method" forState:UIControlStateNormal];
-    [button setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
-    [button addTarget:self action:@selector(eventCrashOnStoreWeak:) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:button];
+    UIButton* buttonCrash = [UIButton buttonWithType:UIButtonTypeSystem];
+    buttonCrash.frame = CGRectMake(0, 100, 380, 50);
+    [buttonCrash setTitle:@"Click Me Crash on objc_initWeak method" forState:UIControlStateNormal];
+    [buttonCrash setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+    [buttonCrash addTarget:self action:@selector(eventCrashOnStoreWeak:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:buttonCrash];
     
     UIButton* buttonDismiss = [UIButton buttonWithType:UIButtonTypeSystem];
     buttonDismiss.frame = CGRectMake(0, 150, 180, 50);
@@ -32,16 +32,22 @@ bool _objc_rootIsDeallocating(id _Nonnull obj);
     [buttonDismiss setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
     [buttonDismiss addTarget:self action:@selector(eventDismissKeyboard:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:buttonDismiss];
-    
-    
+
     // TextFields
     UITextField* textField = [[UITextField alloc] init];
-    textField.frame = CGRectMake(8, 200, 380, 50);
-    [self.view addSubview:textField];
+    textField.frame = CGRectMake(8, 200, 300, 50);
     [textField setBorderStyle:UITextBorderStyleLine];
+    [self.view addSubview:textField];
+    /**
+            if y is 320, not 200, will not crash ...... fucking apple ...
+             so the crash relative to to textfield's y position ......
+     **/
     
-    
-    
+    // Labels
+    UILabel* label = [[UILabel alloc] initWithFrame:CGRectMake(8, 380, 350, 250)];
+    label.numberOfLines = 8;
+    label.text = @" 1. Input some words using a un-official keyboard/input method, then dimiss the keyboard. \n 2. Double tap the textfield for showing up the un-official keyboard, crash will appear. \n 3. Note the UITextField y position should on the top half of the screen.";
+    [self.view addSubview:label];
 }
 
 #pragma mark - Event
@@ -61,11 +67,9 @@ bool _objc_rootIsDeallocating(id _Nonnull obj);
 
 #pragma mark - Category UITouch
 
-// UITouch
 @interface UITouch (FixCrashOnInputKeyboard)
 
 @end
-
 
 @implementation UITouch (FixCrashOnInputKeyboard)
 
@@ -77,10 +81,11 @@ static BOOL (*originalImpl)(id, SEL, UIResponder*, UIResponder*, UIEvent* ) = ni
     NSString* responderClassName = NSStringFromClass([arg2 class]);
     if ([responderClassName isEqualToString:@"_UIRemoteInputViewController"]) {
         if (_objc_rootIsDeallocating(arg2)) {
+            NSLog(@"BingGo a deallocating object ...");
             return true;
         }
     }
-    
+
     BOOL retVal = FALSE;
     if (originalImpl == nil) {
         ObjcSeeker *seeker = [[ObjcSeeker alloc] init];
@@ -90,7 +95,7 @@ static BOOL (*originalImpl)(id, SEL, UIResponder*, UIResponder*, UIEvent* ) = ni
         [seeker seekOriginalMethod:self selector:_cmd];
         originalImpl = (BOOL (*)(id, SEL, UIResponder*, UIResponder*, UIEvent* ))seeker.impl;
     }
-    
+
     if (originalImpl != nil) {
         retVal = originalImpl(self, _cmd, arg1, arg2, arg3);
     }
@@ -101,53 +106,34 @@ static BOOL (*originalImpl)(id, SEL, UIResponder*, UIResponder*, UIEvent* ) = ni
 @end
 
 
-/**
+
  
 #pragma mark - Categories For Checking
 
-// NSSet
-@interface NSSet (CheckingIssue)
+//@interface NSSet (CheckingIssue)
+//
+//@end
+//
+//@implementation NSSet (CheckingIssue)
+//
+//- (void)enumerateObjectsUsingBlock:(void (NS_NOESCAPE ^)(id obj, BOOL *stop))block {
+//    NSArray* array = [self allObjects];
+//    NSLog(@"enumerate enumerateObjectsUsingBlock: %ld, %@", [self count], array);
+//
+//    [ObjcUtil invokeOriginalMethod:self selector:_cmd completion:^(Class clazz, IMP _Nonnull impl) {
+//        ((void (*)(id, SEL, void (NS_NOESCAPE ^)(id obj, BOOL *stop)  ))impl)(self, _cmd, ^(id obj, BOOL *stop){
+//            NSLog(@"enumerating obj: %@", obj);
+//            block(obj, stop);
+//        });
+//    }];
+//}
+//
+//- (void)enumerateObjectsWithOptions:(NSEnumerationOptions)opts usingBlock:(void (^ NS_NOESCAPE)(id _Nonnull obj, BOOL * _Nonnull stop))block {
+//    NSLog(@"enumerate enumerateObjectsWithOptions: %ld", [self count]);
+//    [ObjcUtil invokeOriginalMethod:self selector:_cmd completion:^(Class clazz, IMP _Nonnull impl) {
+//        ((void (*)(id, SEL, NSEnumerationOptions, void (^ NS_NOESCAPE)(id _Nonnull obj, BOOL * _Nonnull stop) ))impl)(self, _cmd, opts, block);
+//    }];
+//}
+//
+//@end
 
-@end
-
-@implementation NSSet (CheckingIssue)
-
-- (void)enumerateObjectsUsingBlock:(void (NS_NOESCAPE ^)(id obj, BOOL *stop))block {
-    NSArray* array = [self allObjects];
-    NSLog(@"enumerate enumerateObjectsUsingBlock: %ld, %@", [self count], array);
-    
-    [ObjcUtil invokeOriginalMethod:self selector:_cmd completion:^(Class clazz, IMP _Nonnull impl) {
-        ((void (*)(id, SEL, void (NS_NOESCAPE ^)(id obj, BOOL *stop)  ))impl)(self, _cmd, ^(id obj, BOOL *stop){
-            NSLog(@"enumerating obj: %@", obj);
-            block(obj, stop);
-        });
-    }];
-}
-
-- (void)enumerateObjectsWithOptions:(NSEnumerationOptions)opts usingBlock:(void (^ NS_NOESCAPE)(id _Nonnull obj, BOOL * _Nonnull stop))block {
-    NSLog(@"enumerate enumerateObjectsWithOptions: %ld", [self count]);
-    [ObjcUtil invokeOriginalMethod:self selector:_cmd completion:^(Class clazz, IMP _Nonnull impl) {
-        ((void (*)(id, SEL, NSEnumerationOptions, void (^ NS_NOESCAPE)(id _Nonnull obj, BOOL * _Nonnull stop) ))impl)(self, _cmd, opts, block);
-    }];
-}
-
-@end
-
-// NSCountedSet
-@interface NSCountedSet (CheckingIssue)
-
-@end
-
-@implementation NSCountedSet (CheckingIssue)
-
-- (void)addObject:(id)object {
-    NSLog(@"enumerate addObject before: %ld", [self count]);
-    [ObjcUtil invokeOriginalMethod:self selector:_cmd completion:^(Class clazz, IMP _Nonnull impl) {
-        ((void (*)(id, SEL, id))impl)(self, _cmd, object);
-    }];
-    NSLog(@"enumerate addObject after: %ld", [self count]);
-}
-
-@end
-
-**/
